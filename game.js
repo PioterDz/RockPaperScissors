4c3c6b04a87e7ffe52379b38ea76b9b1c9771408
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-02-26 22:08:03 +0100

    make game function work at basic, delete old version

--- game.js
+++ game.js
@@ -8,27 +8,129 @@ var rock = document.getElementById('rock');
 var newGame = document.getElementById('new-game');
 var playerResult = document.getElementById('player-score');
 var computerResult = document.getElementById('computer-score');
-var maxRounds = 0;
+var maxRounds;
 var currentRound = 0;
 var playerScore = 0;
 var computerScore = 0;
 var playerChoice;
 var computerChoice;
 var winner;
-var computerLotto = Math.round((Math.random() * [2]) + 1);
 
+
+function getComputerChoice() {
+    var choices = ['rock', 'paper', 'scissors'];
+    var randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function win() {
+    playerScore++;
+    playerResult.innerHTML = playerScore;
+    console.log('win');
+}
+function lose() {
+    computerScore++;
+    computerResult.innerHTML = computerScore;
+    console.log('lose');
+}
+function draw() {
+    console.log('draw');
+}
+
+function game(playerChoice) {
+    computerChoice = getComputerChoice();
+    switch(playerChoice + ' ' + computerChoice) {
+        case 'rock scissors':
+        case 'paper rock':
+        case 'scissors paper':
+            win();
+            break;
+        case 'rock paper':
+        case 'paper scissors':
+        case 'scissors rock':
+            lose();
+            break;
+        case 'rock rock':
+        case 'paper paper':
+        case 'scissors scissors':
+            draw();
+            break;
+    }
+}
+
+
+function main() {
+    rock.addEventListener('click', function(){
+        game('rock');
+    })
+    scissors.addEventListener('click', function(){
+        game('scissors');
+    })
+    paper.addEventListener('click', function(){
+        game('paper');
+    })
+}
+
+main();
+
+
+
+
+
+
+
+/* old version
+// init function 
+newGame.addEventListener('click', function gameInit() {
+    maxRounds = prompt('How many rounds you want to play?');
+    alert('Choose the paper, scissors or rock');
+    console.log('new game ' + maxRounds);
+    eventListeners();
+});
+
+function eventListeners() {
+
+    paper.addEventListener('click', function playerMove() {
+        playerChoice === 'paper';
+        console.log(playerChoice);
+
+    });
+        
+    rock.addEventListener('click', function playerMove() {
+        playerChoice === 'rock';
+        console.log(playerChoice);
+
+    });
+        
+    scissors.addEventListener('click', function playerMove() {
+        playerChoice === 'scissors';
+        console.log(playerChoice);
+
+    });
+    
+}
+function lotto() {
+    computerLotto = Math.round((Math.random() * [2]) + 1);
+    console.log(computerLotto);
+    return computerLotto;
+}
 // get computer's choice
-function getComputerChoice(computerLotto) {
-    if (computerLotto == 1) {
+function getComputerChoice() {
+    lotto();
+    if (computerLotto === 1) {
         computerChoice === 'paper';
+        return computerChoice;
     }
-    else if (computerLotto == 2) {
+    else if (computerLotto === 2) {
         computerChoice === 'rock';
+        return computerChoice;
     }
-    else if (computerLotto == 3) {
+    else if (computerLotto === 3) {
         computerChoice === 'scissors';
+        return computerChoice;
     }
-    console.log('computerLotto =' + computerLotto);
+    console.log(computerChoice);
+    
 }
 
 // win, lose or tie function
@@ -53,60 +155,36 @@ function tie(playerChoice, computerChoice) {
     computerResult.innerHTML = computerScore;
 }
 
-
+function whoWinner() {
+    if (playerScore > computerScore) {
+        winner === 'Player';
+    }
+    else if (playerScore < computerScore) {
+        winner === 'Computer';
+    }
+    return winner;
+}
 // game over function
 function gameOver() {
-    function whoWinner(playerScore, computerScore) {
-        if (playerScore > computerScore) {
-            winner === 'Player';
-        }
-        else if (playerScore < computerScore) {
-            winner === 'Computer';
-        }
-        return winner;
-    };
+    whoWinner();
     document.write('Game Over.' + winner + 'wins.');
     document.write('Click to play again');
     reloadButton = document.createElement('button');
     reloadButton.onClick = location.reload();
 }
 
-function compareGames () {
-    getComputerChoice()
+function compareGames() {
+    getComputerChoice();
     if (playerChoice === computerChoice) {
-       tie();
+       tie(playerChoice, computerChoice);
    }
    else if (playerChoice === 'paper' && computerChoice === 'rock' || playerChoice === 'rock' && computerChoice === 'scissors' || playerChoice === 'scissors' && computerChoice === 'paper') {
-       win();
+       win(playerChoice, computerChoice);
    }
    else if (playerChoice === 'paper' && computerChoice === 'scissors' || playerChoice === 'rock' && computerChoice === 'paper' || playerChoice === 'scissors' && computerChoice === 'rock') {
-       lose();
+       lose(playerChoice, computerChoice);
    }
 }
 
-// init function 
-newGame.addEventListener('click', function gameInit() {
-    maxRounds = prompt('How many rounds you want to play?');
-    alert('Choose the paper, scissors or rock');
-    console.log('new game ' + maxRounds);
-});
-while (currentRound < maxRounds) {
-    paper.addEventListener('click', function playerMove() {
-        playerChoice === 'paper';
-        function compareGames();
-        console.log(playerChoice);
-    });
-        
-    rock.addEventListener('click', function playerMove() {
-        playerChoice === 'rock';
-        function compareGames();
-        console.log(playerChoice);
-    });
-        
-    scissors.addEventListener('click', function playerMove() {
-        playerChoice === 'scissors';
-        function compareGames();
-        console.log(playerChoice);
-    });
-    function gameOver();
-}
+*/
+
