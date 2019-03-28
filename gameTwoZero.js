8d7bca42d346627764d4ba71439f74599cd2a8f7
Piotr Dzienis <dojlidy.zubr@gmail.com>
2019-03-28 20:38:39 +0100

    copy and paste the last version of the game to new js file

--- gameTwoZero.js
+++ gameTwoZero.js
@@ -1,12 +1,12 @@
 'use strict'
 
 var output = document.getElementById('output');
-var paper = document.getElementById('paper');
-var scissors = document.getElementById('scissors');
-var rock = document.getElementById('rock');
 var newGame = document.getElementById('new-game');
 var playerResult = document.getElementById('player-score');
 var computerResult = document.getElementById('computer-score');
+var paper = document.getElementById('paper');
+var scissors = document.getElementById('scissors');
+var rock = document.getElementById('rock');
 var maxRounds;
 var currentRound = 0;
 var playerScore = 0;
@@ -14,185 +14,158 @@ var computerScore = 0;
 var playerChoice;
 var computerChoice;
 var winner;
+var choices = ['rock', 'paper', 'scissors'];
 //modal
 var endGameModal = document.getElementById('end-game-modal');
-var closeModal = document.getElementById('close-modal');
 var playAgainBtn = document.getElementById('play-again');
-var quitPageBtn = document.getElementById('quit-page');
 var endGameResult = document.getElementById('end-game-result');
 
+initialize();
+
+function initialize() {
+    changeElementsVisibility('none', [rock, paper, scissors]);
+    gameEventListeners();
+    newGame.addEventListener('click', gameInit);
+    modalEventListeners();
+}
+
+function changeElementsVisibility(state, elements) { 
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.display = state;
+    }
+}
+
+
+function gameEventListeners() {
+
+    rock.addEventListener('click', function(){
+        compareBothChoices(choices[0]);
+    })
+    paper.addEventListener('click', function(){
+        compareBothChoices(choices[1]);
+    })
+    scissors.addEventListener('click', function(){
+        compareBothChoices(choices[2]);
+    })
+}
+
+function maxRoundsPrompt() {
+    maxRounds = prompt('How many rounds you want to play?');
+    if (typeof maxRounds == null || typeof maxRounds == undefined || maxRounds == '0' || maxRounds.charAt(0) == '-') {
+
+        alert('Error. Please, write a number greater then 0');
+        maxRoundsPrompt();
+    }
+    else {
+        alert('Choose the paper, scissors or rock');
+    }
+
+}
 
 function gameInit() {
-    params.maxRounds = prompt('How many rounds you want to play?');
-    alert('Choose the paper, scissors or rock');
-    console.log('new game ' + params.maxRounds);
+    resetGame();
+    maxRoundsPrompt();
+    changeElementsVisibility('none', [newGame]);
+    changeElementsVisibility('inline-flex', [rock, paper, scissors]);
 }
-newGame.addEventListener('click', gameInit);
 
 function getComputerChoice() {
-    var choices = ['rock', 'paper', 'scissors'];
     var randomNumber = Math.floor(Math.random() * 3);
     return choices[randomNumber];
 }
 
 function win(playerChoice, computerChoice) {
-    params.playerScore++;
-    params.currentRound++;
-    playerResult.innerHTML = params.playerScore;
-    computerResult.innerHTML = params.computerScore;
+    playerScore++;
+    currentRound++;
+    playerResult.innerHTML = playerScore;
+    computerResult.innerHTML = computerScore;
     output.innerHTML = 'You played ' + playerChoice + ' and computer played ' + computerChoice + '. You won this round.'
-    console.log('win');
-    console.log(currentRound);
+
 }
 function lose(playerChoice, computerChoice) {
-    params.computerScore++;
-    params.currentRound++;
-    computerResult.innerHTML = params.computerScore;
-    playerResult.innerHTML = params.playerScore;
+    computerScore++;
+    currentRound++;
+    computerResult.innerHTML = computerScore;
+    playerResult.innerHTML = playerScore;
     output.innerHTML = 'You played ' + playerChoice + ' and computer played ' + computerChoice + '. You lost this round.'
-    console.log('lose');
-    console.log(currentRound);
+
 }
 function draw(playerChoice, computerChoice) {
-    params.currentRound++;
+    currentRound++;
     output.innerHTML = 'You played ' + playerChoice + ' and computer also played ' + computerChoice + '. Its a draw.'
-    console.log('draw');
-    console.log(currentRound);
+
 }
 
-function playerMove(playerChoice) {
+function compareBothChoices(playerChoice) {
+    
     computerChoice = getComputerChoice();
     switch(playerChoice + ' ' + computerChoice) {
         case 'rock scissors':
         case 'paper rock':
         case 'scissors paper':
-            params.progress.push({
-                roundNumber: currentRound,
-                player: playerChoice,
-                computer: computerChoice,
-                winner: 'Player',
-                playerScore: playerScore,
-                computerScore: computerScore
-            });
             win(playerChoice, computerChoice);
             break;
         case 'rock paper':
         case 'paper scissors':
         case 'scissors rock':
-            params.progress.push({
-                roundNumber: currentRound,
-                player: playerChoice,
-                computer: computerChoice,
-                winner: 'Computer',
-                playerScore: playerScore,
-                computerScore: computerScore
-            });
             lose(playerChoice, computerChoice);
             break;
         case 'rock rock':
         case 'paper paper':
         case 'scissors scissors':
-        params.progress.push({
-            roundNumber: currentRound,
-            player: playerChoice,
-            computer: computerChoice,
-            winner: 'Draw',
-            playerScore: playerScore,
-            computerScore: computerScore
-        });
             draw(playerChoice, computerChoice);
             break;
     }
     checkRounds();
 }
 
-
-rock.addEventListener('click', function(){
-    playerMove('rock');
-})
-scissors.addEventListener('click', function(){
-    playerMove('scissors');
-})
-paper.addEventListener('click', function(){
-    playerMove('paper');
-})
-
-
-// cos co zrobiłem 
-var whatHavePlayerMoveClass = document.querySelectorAll('.player-move');
-
-
-console.log(whatPlayerHasJustClicked + 'playerclicked');
-
-for (i=0 ; i<whatHavePlayerMoveClass.length ; i++) {
-    var whatPlayerHasJustClicked = whatHavePlayerMoveClass[i].getAttribute('data-move');
-    playerMove(whatPlayerHasJustClicked);
-
-}
-
-
-var params = {
-    playerScore: 0,
-    computerScore: 0,
-    maxRounds: '',
-    currentRound: 0,
-    progress: []
-}
-// ---
-
-
 function checkRounds() {
-    if (params.currentRound >= params.maxRounds) {
+    if (currentRound == maxRounds) {
+
         gameOver();
     }
 }
 
 function gameOver() {
     whoWinner();
+    changeElementsVisibility('none', [rock, paper, scissors]);
     endGameModal.style.display = "block";
 }
 
 function whoWinner(winner) {
-    if (params.playerScore > params.computerScore) {
+    if (playerScore > computerScore) {
         winner = 'You win';
     }
-    else if (params.playerScore < params.computerScore) {
+    else if (playerScore < computerScore) {
         winner = 'Computer wins';
     }
-    else if (params.playerScore === params.computerScore) {
+    else if (playerScore === computerScore) {
         winner = 'Its draw';
     }
-    endGameResult.innerHTML = 'Game over, its ' + params.playerScore + ' to ' + params.computerScore + '. ' + winner;
+    endGameResult.innerHTML = 'Game over, its ' + playerScore + ' to ' + computerScore + '. ' + winner;
 }
 
 function resetGame() {
-    params.playerScore = 0;
-    params.computerScore = 0;
-    params.currentRound = 0;
+    playerScore = 0;
+    computerScore = 0;
+    currentRound = 0;
     output.innerHTML = " ";
     playerResult.innerHTML = " ";
     computerResult.innerHTML = " ";
 }
 //modal functions
+function modalEventListeners() {
 
-closeModal.addEventListener('click', function() {
-    endGameModal.style.display = "none";
-})
-
-quitPageBtn.addEventListener('click', function() {
-    endGameModal.style.display = "none";
-})
-
-playAgainBtn.addEventListener('click', function() {
-    resetGame();
-    console.log('reset score' + playerScore + computerScore + currentRound);
-    endGameModal.style.display = "none";
-    gameInit();
-})
-
-document.body.addEventListener('click', function(event) {
-    if (event.target == endGameModal) {
+    playAgainBtn.addEventListener('click', function() {
+        resetGame();
         endGameModal.style.display = "none";
-    }
-})
+        gameInit();
+    })
+
+    document.body.addEventListener('click', function(event) {
+        if (event.target == endGameModal) {
+            endGameModal.style.display = "none";
+        }
+    })
+}
 
